1b8e7a536be2808762810c18046c912203a2357e
Eliane Henriqueta <elianehenriqueta@gmail.com>
2022-09-16 11:03:51 -0300

    Exercicio 03 - Criar classe "Funcionario"

---
+++ Semana 03/Rh/Rh/Models/Funcionario.cs
@@ -0,0 +1,19 @@
+﻿using Rh.Enuns;
+
+namespace Rh.Models
+{
+    public class Funcionario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Senha { get; set; }
+        public Permissoes Permissao { get; set; }
+        public decimal Salario { get; set; }
+
+        public Funcionario()
+        {
+        }
+
+
+    }
+}
