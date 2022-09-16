2bbe00113543641e318e816d97890713bdf582d8
Eliane Henriqueta <elianehenriqueta@gmail.com>
2022-09-16 16:47:19 -0300

    Exercicio 06 - Criar classe estatica Settings

---
+++ Semana 03/Rh/Rh/Security/Settings.cs
@@ -0,0 +1,7 @@
+﻿namespace Rh.Security
+{
+    public class Settings
+    {
+        public static readonly string Secret = "09702c0ec852kj2579a854hg4810db832a8b29ebdc1d7a483cf564tr322iu";
+    }
+}
