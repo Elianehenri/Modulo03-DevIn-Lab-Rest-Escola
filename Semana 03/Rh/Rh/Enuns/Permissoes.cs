7c9cf13266135af6565f63f873c54022bb695b8e
Eliane Henriqueta <elianehenriqueta@gmail.com>
2022-09-16 10:54:39 -0300

    Exercicio 02 - Criar enum "Permissoes"

---
+++ Semana 03/Rh/Rh/Enuns/Permissoes.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Rh.Enuns
+{
+    public enum Permissoes
+    {
+        [Display(Name = "Funcionário")]
+        Funcionario = 1,
+        [Display(Name = "Gerente")]
+        Gerente = 2,
+        [Display(Name = "Administrador")]
+        Administrador = 3
+    }
+}
