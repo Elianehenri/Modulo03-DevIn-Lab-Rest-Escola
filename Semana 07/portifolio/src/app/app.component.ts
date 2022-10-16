78a374abc3bafa35466839e5be5cbbb2316137d9
Eliane Henriqueta <elianehenriqueta@gmail.com>
2022-10-15 22:02:21 -0300

    Exercico03-Landing page (parte 3) - Formbuilder

--- Semana 07/portifolio/src/app/app.component.ts
+++ Semana 07/portifolio/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder,FormControl,FormGroup, FormGroupName } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portifolio';
+
+contato = new FormGroup({
+  nome: new FormControl(),
+  sobrenome:new FormControl(),
+  email:new FormControl(),
+  feedback: new FormControl(),
+
+
+});
+control: FormControl;
+group: FormGroup;
+  constructor(private formBuilder: FormBuilder){
+
+
+      this.control = formBuilder.control({value: 'my val', disabled: true});
+      this.group = formBuilder.group({
+        controller: this.control,
+        controllerTwo: new FormControl('')
+      });
+      console.log(this.contato);
+
+
+
+  }
 }
