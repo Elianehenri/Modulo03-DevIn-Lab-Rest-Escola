269009464bbdbb3f245a1d146a3467bb9a99a00a
Eliane Henriqueta <elianehenriqueta@gmail.com>
2022-10-16 11:00:27 -0300

    Exercicio05-Formulário com Bootstrap-Cursos

---
+++ Semana 07/cursos/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RegistrosComponent } from './registros/registros.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RegistrosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
