38852538864ecf649fbf1b4a015691b582db22d6
Eliane Henriqueta <elianehenriqueta@gmail.com>
2022-10-16 15:49:46 -0300

    Exercicio05-FormGroup e FormControl

--- Semana 07/cursos/src/app/registros/registros.component.ts
+++ Semana 07/cursos/src/app/registros/registros.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-registros',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrosComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formBuilder:FormBuilder) { }
 
   ngOnInit(): void {
+
+  }
+  register = this.formBuilder.group({
+    nome: this.formBuilder.control(''),
+    cargaHoraria: this.formBuilder.control(''),
+    escola: this.formBuilder.control('SENAI/SC - Serviço Nacional de Aprendizagem Industrial'),
+    professor: this.formBuilder.control(''),
+    materia: this.formBuilder.control(''),
+  })
+  showForm(){
+    console.log(this.register);
+
   }
 
 }
